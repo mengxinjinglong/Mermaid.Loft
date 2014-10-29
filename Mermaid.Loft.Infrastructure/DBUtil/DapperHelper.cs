8192ebfa0c1f6307cdbed400e3a43c6271595fc5
mengxinjinglong <mengxinjinglong@163.com>
2014-10-29 17:39:45 +0800

    添加基础类

---
+++ Mermaid.Loft.Infrastructure/DBUtil/DapperHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mermaid.Loft.Infrastructure.DomainBase;
+
+namespace Mermaid.Loft.Infrastructure.DBUtil
+{
+    public class DapperHelper<T>:IRepository<T> where T: EntityBase
+    {
+        private const string CONNECTION = "Data Source=mermaid.db;Version=3;Password=mylofter123;";
+
+
+        public DapperHelper()
+        { 
+            
+        }
+
+        public DbConnection GetConnection()
+        {
+            var sqliteConnection = new SQLiteConnection(CONNECTION);
+            sqliteConnection.Open();
+            return sqliteConnection;
+        }
+
+    }
+}
---
+++ Mermaid.Loft.Infrastructure/DBUtil/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mermaid.Loft.Infrastructure.DomainBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mermaid.Loft.Infrastructure.DBUtil
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+
+    }
+}
