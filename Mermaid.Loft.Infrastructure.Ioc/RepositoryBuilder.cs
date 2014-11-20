1e93b3cb8213d05b52d3d7db907df8f048f20ff0
mengxinjinglong <mengxinjinglong@163.com>
2014-11-21 00:08:15 +0800

    change ioc content

--- Mermaid.Loft.Infrastructure.Ioc/RepositoryBuilder.cs
+++ Mermaid.Loft.Infrastructure.Ioc/RepositoryBuilder.cs
@@ -1,4 +1,7 @@
 ﻿using Autofac;
+using Mermaid.Loft.Infrastructure.Ioc.Categories;
+using Mermaid.Loft.Infrastructure.Ioc.Products;
+using Mermaid.Loft.Infrastructure.Ioc.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +23,16 @@ namespace Mermaid.Loft.Infrastructure.Ioc
         public void RegisterType<T>() where T:class
         {
             _container.RegisterType<T>();
+        }
+
+        public void Initialize()
+        { 
+            _container.RegisterType<CategoryRepositoryImpl>()
+                .As<ICategoryRepository>();
+            _container.RegisterType<ProductRepositoryImpl>()
+                .As<IProductRepository>();
+            _container.RegisterType<UserRepositoryImpl>()
+                .As<IUserRepository>();
         }
     }
 }
